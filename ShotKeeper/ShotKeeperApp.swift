b696d87f16dc2f8d859e9a95eda27d76561537c7
bamin2 <bader.amin@gmail.com>
2026-06-23 00:53:23 +0300

    Initial Commit

---
+++ ShotKeeper/ShotKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShotKeeperApp.swift
+//  ShotKeeper
+//
+//  Created by Bader Amin on 23/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct ShotKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
